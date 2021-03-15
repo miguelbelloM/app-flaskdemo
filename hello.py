56117cc5b1d95d50728b1783753fd8eab504d0e2
miguelbelloM <miguelbellomunevar@gmail.com>
2021-03-14 21:35:40 -0500

    add new API Rest with flask

--- hello.py
+++ hello.py
@@ -1,8 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+stores = [
+    {
+        'name': 'My Wondersul Store',
+        'items': [
+            {   
+                'name': 'My Item',
+                'price': 15.99
+            }            
+        ]
+    }
+]
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+@app.route('/store')
+def get_store():
+    return jsonify({'sotores': stores})
+
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+@app.route('/store/<string:name>/item')
+def get_intem_in_store(name):
+    pass
+
