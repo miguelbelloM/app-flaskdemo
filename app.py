d3bae14cdd51019c0cfc624538c22378e6b4c526
miguelbelloM <miguelbellomunevar@gmail.com>
2021-03-16 06:41:48 -0500

    create code endpoint and filter the item

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        return {'item': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
     
     def post(self, name):
         item = {
@@ -17,6 +20,6 @@ class Item(Resource):
             'price': 12.00
         }
         items.append(item)
-        return item
+        return item, 201
 
 api.add_resource(Item, '/item/<string:name>')
