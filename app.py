42ab74a145e42c57cf68ec8cf5f0707ba599079a
miguelbelloM <miguelbellomunevar@gmail.com>
2021-03-17 07:45:25 -0500

    add reqparse in put method

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ jwt = JWT(app, aunthenticate, identity)
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="this field cannot be left blank!"
+    )
+        
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -22,8 +30,8 @@ class Item(Resource):
     def post(self, name):
         if next(filter(lambda item: item['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists".format(name)}, 400
-
-        data = request.get_json()
+        
+        data = Item.parser.parse_args()
         # Variables
         price = data['price']
         
@@ -40,14 +48,7 @@ class Item(Resource):
         return {'message': 'item deleted'}
     
     def put(self,name):
-        parser = reqparse.RequestParser()
-        parser.add_argument(
-            'price',
-            type=float,
-            required=True,
-            help="this field cannot be left blank!"
-        )
-        data = parser.parse_args()
+        data = Item.parser.parse_args()
         
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
