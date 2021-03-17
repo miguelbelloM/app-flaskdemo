ec342f83acf836eb9984f484b6b70abb4f6f93c8
miguelbelloM <miguelbellomunevar@gmail.com>
2021-03-17 07:20:03 -0500

    check  parser.parse_args()

--- app.py
+++ app.py
@@ -43,11 +43,11 @@ class Item(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument(
             'price',
-            type = float,
-            required = True,
-            help = "this field cannot be left blank!"
+            type=float,
+            required=True,
+            help="this field cannot be left blank!"
         )
-        data = request.get_json()
+        data = parser.parse_args()
         
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
